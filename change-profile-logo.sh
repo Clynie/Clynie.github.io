3ab750359ec44519fae00d614c945450902c770e
Clynie <lyniedairce@163.com>
2020-02-12 20:11:59 +0800

    modify logo

--- change-profile-logo.sh
+++ change-profile-logo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-convert $1 -resize 16x16 favicon.ico
-convert $1 -resize 32x32 favicon.png
-convert $1 -resize 72x72 assets/img/favicons/apple-icon-72x72.png
-convert $1 -resize 114x114 assets/img/favicons/apple-icon-114x114.png
-convert $1 -resize 144x144 assets/img/favicons/apple-icon-144x144.png
-convert $1 -resize 72x72 assets/img/favicons/apple-icon-precomposed.png
-convert $1 assets/img/favicons/logo.png
-convert $1 assets/img/logo.png
+sips -Z 16  logo.png --out favicon.ico
+sips -Z 32  logo.png --out favicon.png
+sips -Z 72  logo.png --out assets/img/favicons/apple-icon-72x72.png
+sips -Z 114 logo.png --out assets/img/favicons/apple-icon-114x114.png
+sips -Z 144 logo.png --out assets/img/favicons/apple-icon-144x144.png
+sips -Z 72  logo.png --out assets/img/favicons/apple-icon-precomposed.png
+sips assets/img/favicons/logo.png
+sips assets/img/logo.png
